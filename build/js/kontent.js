cbd65809b8ea932a4960cc6cc747b2e31302c0b0
joeos <gijoe0000@gmail.com>
2018-07-24 12:44:06 +0100

    netlify test6 added google maps

--- build/js/kontent.js
+++ build/js/kontent.js
@@ -116,7 +116,7 @@ var $homeBut=$('.homeButton');
 // load full gallery page using AJAX
 var $galBut= $('.galButton');
     $galBut.click(function(){
-        $('main').load('gallery.html main > *', null, onLoadGallery);
+        $('main').load('gallery.html #gallery > *', null, onLoadGallery);
         console.log($galBut);
 });
 
@@ -184,6 +184,55 @@ submittednews.click(function(){
     alert("Submit Success!");
     console.log("submit clicked");
 });
+
+
+
+/**** Map *****/
+
+
+
+
+var latText=document.getElementById('latitude');
+var longText=document.getElementById('longitude');
+var lat;
+var lng;
+var latlng={};
+var map;
+var marker;
+
+
+var navi=navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+
+function successCallback(pos){
+lat=pos.coords.latitude;
+lng=pos.coords.longitude;
+
+latText.value=lat.toFixed(3);
+longText.value=lng.toFixed(3);
+latlng={Lat: lat, Lng: lng};
+console.log('No error..');
+}
+
+
+
+function errorCallback(){
+    console.log('error!!!!!!');
+}
+
+
+
+
+function initMap(){
+    latlng=new google.maps.LatLng(lat,  lng);
+    map=new google.maps.Map(document.getElementById('mapDiv'), {zoom: 16, center: latlng});
+    marker=new google.maps.Marker({position: latlng, map: map});
+}
+
+
+
+
+
 
 
 
